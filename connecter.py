c5666bf356a148100a04d1bc83473d2cfe5c79fc
Gokhan Karakas <gokhankarakas@linux.erciyes.edu.tr>
2015-09-14 20:49:24 +0300

    completed test
    completed test connections.

--- connecter.py
+++ connecter.py
@@ -115,6 +115,245 @@ try:
         print "Success value: %s" % successvalue
     session_id = xlmTag.getAttribute("session-id")
     if(vervar == "on"):
+        print 'success value %s' % successvalue
+    session_id=xlmTag.getAttribute('session-id')
+    if(vervar=='on'):
+        print 'session-id: %s' % session_id
+except:
+    print 'Login Response exception occured'
+    exit(-1)
+
+#check success
+
+if successvalue==0 :
+    print 'Login Error\n'
+    exit(-1)
+
+#create the site in Nexpose
+nexpose_site_name= datetime.datetime.now().strftime('%y-%m-%d-%H-%M') + '_Nexpose_test'
+host_string=''
+
+if (regchecka ==0):
+    host_string= '<host>%s</host>' % ipvar
+
+if(regcheckb==0):
+    of= open(listvar, 'r').readlines()
+    list=[]
+    for lines in of:
+        ip=lines.strip()
+        hostag='<host>%s</host>' % ip
+        list.append(hostag)
+    hosts_string=list
+
+#building site creation request
+xml_request = """<SiteSaveRequest session-id='%s'>""" % session_id + """<Site id+"-1" name="%s" description="Quick Scan">""" % nexpose_site_name + """<Hosts>""" +str(hosts_string) +"""</Hosts> <Credentials></Credentials><Alerting></Alerting> <ScanConfig ConfigID="-1" name="Special Example" templateID="full-audit"></ScanConfig></Site> </SiteSaveRequest>"""
 
 
-https://github.com/lordsaibat/nexpose_python_client/blob/master/nexpose.py
+headers = {"Content-type": "text/xml"}
+
+print 'Creating the site'
+
+try:
+    c.request("POST", "/api/1.2/xml", xml_request, headers)
+    response = c.getresponse()
+except:
+    print "Problem making the SiteSaveRequest."
+    exit(-1)
+
+if(vervar="on"):
+    print "Response status: %s % Response reason: %s" % (response.status, response.reason)
+data = response.read()
+
+if(vervar="on"):
+    print('Data received: %s' % data)
+
+#take the data received and look for what we need
+
+dom= parseString(data)
+try:
+    xmlTag= dom.getElementsByTagName("SiteSaveResponse")[0]
+    successvalue=xmlTag.getAttribute("success")
+    site_id=xmlTag.getAttribute("site-id")
+except:
+    print "Results from the SiteSaveResponse returned unexpected result"
+    exit(-1)
+
+#check success
+if(successvalue==0):
+    print 'Site Creation Error\n'
+    exit(-1)
+
+#wait for site to be created
+
+time.sleep(5)
+
+#scan site request
+
+xml_request="""<SiteScanRequest session-id="%s" site-id="%s"></SiteScanRequest>""" % (session_id, site_id)
+headers= {"Content-type":"text/xml"}
+
+print "Telling Nexpose to scan the site created"
+
+try:
+    c.request("POST", "/api/1.2/xml", xml_request, headers)
+    response=c.getresponse()
+except:
+    print "Problem making the SiteScanRequest"
+    exit(-1)
+
+if(vervar="on"):
+    print "Response status: %s, Response reason: %s" % (response.status, response.reason)
+    data=response.read()
+if(vervar="on"):
+    print "Data received: %s" % data
+
+
+# take the data received and look for what we need
+dom = parseString(data)
+try:
+    xmlTag=dom.getElementsByTagName("SiteScanResponse")[0]
+    successvalue=xmlTag.getAttribute("succes")
+except:
+    print "Results from the SiteSaveResponse returned an unexpected result"
+    exit(-1)
+
+#check success
+if(successvalue==0):
+    print "Site scan error Error\n"
+    exit(-1)
+
+#Take scan-id and engine-id
+#take the data received and look for what we need
+
+try:
+    dom = parseString(data)
+    xmlTag= dom.getElementsByTagName("SiteScanResponse")[0]
+    successvalue=xmlTag.getAttribute("success")
+
+    xmlTag2 = dom.getElementsByTagName("Scan")[0]
+    scan_id = xmlTag2.getAttribute("scan-id")
+    engine_id = xmlTag2.getAttribute("engine-id")
+except:
+    print 'Results from the SiteSaveResponse returned an unexpected result'
+    exit(-1)
+
+
+#check success
+if(successvalue==0):
+    print 'Site scan error Error\n'
+    exit(-1)
+
+#test if scan is complete
+
+exception= ''
+status='running'
+while(exception=="" and status=="running"):
+    #reset status
+    status="stopped"
+    exception=""
+    successvalue=""
+
+    xml_request ="""<ScanStatusRequest session-id="%s" scan-id="%s"></ScanStatusRequest>""" % (session_id, scan_id)
+    headers={"Content-type":"text/xml"}
+    print "Checking on the status of the scan"
+
+    c=httplib.HTTPSConnection(nexposeip, 3780)
+    try:
+        c.request("POST", "/api/1.2/xml", xml_request, headers)
+        response=c.getresponse()
+        if(vervar=='on'):
+            print "Response status: %s, Response reason: %s" % (response.status, response.reason)
+        data = response.read()
+        if  (vervar='on'):
+            print 'Data recieved: %s ' % data
+    except:
+        print "ScanStatusRequest exception"
+        status= "stopped"
+        exception='stop'
+        pass
+
+#take the data received and look for what we need
+    dom = parseString(data)
+    try:
+        xmlTag = dom.getElementsByTagName("ScanStatusResponse")[0]
+        successvalue=xmlTag.getAttribute("success")
+        status= xmlTag.getAttribute("status")
+    except:
+        print "Result from the ScanStatusResponse returned an unexpected result"
+        exit(-1)
+
+    if(dom==""):
+        print "No data"
+        status="stopped"
+
+    #check success
+    if (successvalue==0):
+        print "error scanning\n"
+        exit(-1)
+
+    #check exception
+    #print exception
+    if (exception == "stopped"):
+        status = "stop"
+
+    #print status
+    print "Scan still running"
+
+    #kill old connections
+    c.close()
+    time.sleep(10)
+
+
+#scanning done
+print "Scanning done"
+
+#generate ad-hoc report
+
+con = httplib.HTTPSConnection(nexposeip, 3780)
+xml_request="""<ReportAdhocGenerateRequest session-id="%s"><AdhocReportConfig template-id="audit-report" format="pdf"><Filters><filter type="scan" id="%s"></filter></Filters></AdhocReportConfig></ReportAdhocGenerateRequest>""" % (session_id, scan_id)
+headers={"Content-type":"text-xml"}
+
+print "Requesting an adhoc report"
+
+try:
+    con.request("POST", "/api/1.2/xml", xml_request, headers)
+    response=con.getresponse()
+except:
+    print "Results from the ReportAdhocGenerateRequest returned unexpected result"
+    exit(-1)
+
+if (vervar=='on'):
+    print "Response status: %s, Response reason: %s" % (response.status, response.reason)
+data = response.read()
+if (vervar=="on"):
+    print 'Data recieved %s' % data
+
+#take the data received and look for what we need
+#need to cut out the base64 content
+base64_identifier="--AxB9sl3299asdjvbA"
+
+content_start= "Content-Transfer-Encoding: base64"
+
+start_of_base64 = data.rfind(content_start)
+
+#print start_of_base64
+
+end_of_base64=data.rfind(base64_identifier)
+#print end_of_base64
+
+base64_data = data[start_of_base64+len(content_start)+2:end_of_base64]
+#print  base64_data
+
+#decode the data to write
+decoded= base64.b64decode(base64_data)
+
+print "Saving the report"
+
+#prepare to wrhite the file
+if(outputvar != None):
+    try:
+        f = open(outputvar, "w")
+        f.write(decoded)
+    except:
+        print "Could not write the file"
+        exit(-1)
